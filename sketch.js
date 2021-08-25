a140f3dd8a74d5f61ed1a29b61013bc45765756c
snkatariyadnd12 <83283682+snkatariyadnd12@users.noreply.github.com>
2021-08-25 18:41:34 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,84 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Body
+
+var roof
+var bobObject1,bobObject2,bobObject3,bobObject4,bobObject5
+var rope1,rope2,rope3,rope4,rope5
+
+function preload(){
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+	
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	roof = new Roof(350,100,300,30);
+	World.add(world,roof);
+
+	bobObject1 = new bobObject(250,300);
+	bobObject2 = new bobObject(300,300);
+	bobObject3 = new bobObject(350,300);
+	bobObject4 = new bobObject(400,300);
+	bobObject5 = new bobObject(450,300);
+
+	rope1 = new Rope(bobObject1.body,roof.body,roof.body,-100,0);
+	World.add(world,rope1);
+
+	rope2 = new Rope(bobObject1.body,roof.body,roof.body,-50,0);
+	World.add(world,rope2);
+
+	rope3 = new Rope(bobObject1.body,roof.body,roof.body,0,0);
+	World.add(world,rope3);
+
+	rope4 = new Rope(bobObject1.body,roof.body,roof.body,+50,0);
+	World.add(world,rope4);
+
+	rope5 = new Rope(bobObject1.body,roof.body,roof.body,+100,0);
+	World.add(world,rope5);
+
+    Engine.run(engine);
+
+	
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background("white");
+  Engine.update(engine)
+
+  roof.display();
+
+  bobObject1.display();
+  bobObject2.display();
+  bobObject3.display();
+  bobObject4.display();
+  bobObject5.display();
+
+  rope1.display();
+  rope2.display();
+  rope3.display();
+  rope4.display();
+  rope5.display();
+  
+  drawSprites();
+ 
+}
+function keyPressed(){
+	if(keyCode===UP_ARROW){
+		Matter.body.applyForce(bobObject1.body,bobObject1.body.position,{x:-730,y:0});
+	}
+}
+
+
+
